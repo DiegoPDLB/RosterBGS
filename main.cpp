d1c763de168ca8ea59dd95ed00970c68b9351a5a
Diego Ponce de León Betanzos <111619317+DiegoPDLB@users.noreply.github.com>
2023-06-15 07:08:25 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
+#include <iostream>
+#include <Windows.h>
 #include "metodos.cpp"
 /// En el include ponemos el archivo de metodos.cpp para jalar toda la informacion 
 
 int main(){
     /// Dentro del main solo presentamos las opciones que el usuario podra tomar con sus respectivos if
+    SetConsoleOutputCP(CP_UTF8);
     int opcion = 0;
     cout << "Desea conocer el Roster?" << endl;
     cout << "Si = 1 | No = 0" << endl;
@@ -11,6 +14,8 @@ int main(){
         int opcion1 = 0;
         equipo borregos("Mayor Onefa" , "Borregos" , "Queretaro" , 76130);
         borregos.crearEquipo();
+        borregos.cargarJugadores();
+        borregos.cargarCoaches();
         while (true){
                 cout << "__________________________________" << endl;
                 cout << "Opciones de roster: " << endl;
@@ -19,6 +24,8 @@ int main(){
                 cout << "3. Buscar Experiencia" << endl;
                 cout << "4. Buscar Jugador" << endl;
                 cout << "5. Buscar Rating" << endl;
+                cout << "6. AÃ±adir Jugador" << endl;
+                cout << "7. Salir" << endl;
                 cout << "__________________________________" << endl;
 
                 cout << "Opcion: ";
@@ -37,7 +44,7 @@ int main(){
 
                 else if (opcion1 == 3){
                         int exp;
-                        cout << "Cuantos aos de experiencia? ";
+                        cout << "Cuantos aÃ±os de experiencia? ";
                         cin >> exp;
                         borregos.buscarExp(exp);
                 }
@@ -58,7 +65,14 @@ int main(){
                         cin >> ra;
                         borregos.buscarRating(ra); 
                 }
-        break;   // Antes tenia un bucle porque no habia escrito este break     
+
+                else if (opcion1 == 6){
+                        borregos.anadirJugador(); 
+                }
+
+                else if (opcion1 == 7){
+                        break;; 
+                }   
         }
     }
     else{
@@ -66,4 +80,3 @@ int main(){
     }
         
 }
-
